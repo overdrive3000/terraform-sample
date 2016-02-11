21c6d51af63e41c24e5a271dbfa82cdae5d386ea
Juan Mesa <linuxven@gmail.com>
2016-02-11 02:15:19 -0500

    Added variables map for AWS AZ

--- variables.tf
+++ variables.tf
@@ -23,27 +23,34 @@ variable "ecs-ami" {
         }
 }
 
+variable "availability_zones" {
+        default = {
+                "us-east-1" = "us-east-1b,us-east-1c"
+                "us-west-1" = "us-west-1b,us-west-1c"
+                "us-west-2" = "us-west-2a,us-west-2b"
+                "eu-west-1" = "eu-west-1a,eu-west-1b"
+                "eu-central-1" = "eu-central-1a,eu-central-1b"
+                "ap-northeast-1" = "ap-northeast-1a,ap-northeast-1c"
+                "ap-southeast-1" = "ap-southeast-1a,ap-southeast-1b"
+                "ap-southeast-2" = "ap-southeast-2a,ap-southeast-2b"
+        }
+}
+
 variable "vpc_cidr" {
         description = "CIDR for the VPC"
         default = "10.130.0.0/16"
 }
 
-variable "public0_subnet_cidr" {
-        description = "CIDR for Public Subnet 0"
-        default = "10.130.0.0/24"
+variable "public_subnet_cidr" {
+        default = { 
+                "0" = "10.130.0.0/24"
+                "1" = "10.130.1.0/24"
+        }
 }
 
-variable "public1_subnet_cidr" {
-        description = "CIDR for Public Subnet 1"
-        default = "10.130.1.0/24"
-}
-
-variable "private0_subnet_cidr" {
-        description = "CIDR for Private Subnet 0"
-        default = "10.130.2.0/24"
-}
-
-variable "private1_subnet_cidr" {
-        description = "CIDR for Private Subnet 1"
-        default = "10.130.3.0/24"
+variable "private_subnet_cidr" {
+        default = { 
+                "0" = "10.130.2.0/24"
+                "1" = "10.130.3.0/24"
+        }
 }
