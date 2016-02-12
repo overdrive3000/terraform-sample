78e86abb0fb2c136c668f1b6b86bfabaf8563a73
Juan Mesa <linuxven@gmail.com>
2016-02-12 10:25:35 -0500

    Removed unsupported output

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
 #### VPC Outputs ####
 output "public_subnets" {
-        value = "${join("," aws_subnet.public.*.id)}"
+        value = "${join(",", aws_subnet.public.*.id)}"
 }
 
 output "private_subnets" {
-        value = "${join("," aws_subnet.private.*.id)}"
+        value = "${join(",", aws_subnet.private.*.id)}"
 }
 
 output "nat_private_ip" {
-        value = "${aws_nat_gateway.igw.private_ip}"
+        value = "${aws_nat_gateway.nat_gw.private_ip}"
 }
 
 output "nat_public_ip" {
-        value = "${aws_nat_gateway.igw.public_ip}"
+        value = "${aws_nat_gateway.nat_gw.public_ip}"
 }
 
 ### RDS Outputs ####
 output "rds_endpoint" {
-        value = "${aws_rds_instance.wordpress-db.address}"
-}
-
-output "rds_status" {
-        value "${aws_rds_instance.wordpress-db.status}"
+        value = "${aws_db_instance.wordpress-db.address}"
 }
